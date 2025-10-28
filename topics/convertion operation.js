7f17c5dd958c1331656a160dd2465dfdc341f323
Tushar8899 <tushartyagi00001@gmail.com>
2025-10-28 23:33:56 +0530

    Update convertion operation.js

--- topics/convertion operation.js
+++ topics/convertion operation.js
@@ -2,3 +2,13 @@
 //"33abc" => NaN
 // true => 1 ; 
 // false => 0;
+ 
+let score = "tushar";
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+console.log(typeof score);
+
+// we can use three as convertion 1st number 2nd boolean 3rd string 
+
+
