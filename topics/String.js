f04851f7df42f6454fb2a8187dc58a2f0938506c
Tushar8899 <tushartyagi00001@gmail.com>
2025-11-05 21:55:13 +0530

    Update String.js

---
+++ topics/String.js
@@ -0,0 +1,10 @@
+const name = "tushar";
+console.log(`hello i am ${"tushar"} and roll no is ${21}`);
+
+const gameName = new String('Tushar')
+console.log(gameName[0])
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(5));
+
+
