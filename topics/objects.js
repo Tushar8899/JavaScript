505beb65e83a184d07a2fcb950b6d15d93aa4810
Tushar8899 <tushartyagi00001@gmail.com>
2025-11-14 22:39:21 +0530

    Update objects.js

--- topics/objects.js
+++ topics/objects.js
@@ -16,13 +16,26 @@ const student_info = {
 
 }
 
-console.log(student_info.name); // this is dot method 
-console.log(student_info["address"]); // this is the best way to get object values , this is square notation
-console.log(student_info[mySym]); // to access symbol use this 
+// console.log(student_info.name); // this is dot method 
+// console.log(student_info["address"]); // this is the best way to get object values , this is square notation
+// console.log(student_info[mySym]); // to access symbol use this 
 
 student_info.email = "tushar@8899.com"; // = is used to update the value of object key
-console.log(student_info.email);
+// console.log(student_info.email);
 
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
+// const laptop_name = new Object();  // this is a singleton object 
+// const laptop_name = {};   // this is non singleton object 
 
+const obj1 = {1: "a", 2: "b"};
+const obj2 = {3: "a", 4: "b"};
+const obj3 = {5: "a", 6: "b"};
+
+const obj4 = Object.assign({}, obj1,obj2,obj3);  //.assign is used to combine objects , but we use spread(...) instead of assign 
+// console.log(obj4);
+
+// console.log(Object.keys(student_info)); // it gives you result in array 
+// console.log(Object.values(student_info)); // it gives you result in array 
+console.log(Object.entries(student_info)); 
 
