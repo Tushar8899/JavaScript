15820c0b745ed3c778d257f87ab6f6ed36da327f
Tushar8899 <tushartyagi00001@gmail.com>
2025-11-19 21:16:08 +0530

    Create scopes.js

---
+++ topics/scopes.js
@@ -0,0 +1,38 @@
+let a = 2;                   // it is the global scope 
+
+if (true){
+  let a = 4                     // it is the blocked scope 
+  const b = 8
+  console.log( "inner" , a);      // here display the blocked scope
+  
+
+}
+console.log(a);       // here display the global scope 
+
+//++++++++++++++++++++++  nested scope +++++++++++++++++++++++++++++++++++++
+
+if (true){
+  const username = "tushar"
+
+  if (username==="tushar"){
+    const website = "portfolio"
+    console.log(username);        // it display because it is in the scope 
+    
+  }
+  // console.log(website);          // it can not display because it is out of scope 
+  
+}
+
+
+//+++++++++++++++++++++++++++ function and expression ++++++++++++++++++++++++++++++++++
+
+function one (num){         // it is called function 
+  return num+1
+}
+
+console.log(one(3));             // it can be access before initialisation 
+
+const addTwo = function(num){                     // it is called expression
+  return num + 3
+}
+console.log(addTwo(3));               // it cannot be access before initialisation 
