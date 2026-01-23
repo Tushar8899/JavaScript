f2210704bbaca0c627f743a17f42628bb10f5bdc
Tushar8899 <tushartyagi00001@gmail.com>
2026-01-23 15:03:16 +0530

    Update Arrowfuntion.js

--- topics/Arrowfuntion.js
+++ topics/Arrowfuntion.js
@@ -1,4 +1,19 @@
 const user = {
   username: "tushar",
-  price: 999
+  price: 999,
+
+  welcomemessage : function() {
+    console.log (`${this.username}, welcome to website `);   // here This. is used for current context or inside the curly brases. and this is only used in object 
+  } 
 }
+// user.welcomemessage()
+// user.username = "sam"   // here current context is changed 
+// user.welcomemessage() 
+
+// console.log(this);   // this is empty in the local computer because it use node , and in the browser it shows window because window is the global object 
+
+function coffee(){
+  console.log(this);
+  
+}
+coffee()
