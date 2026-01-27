5acf7b16fa6f74d9557584457e77777d2bc82bc3
Tushar8899 <tushartyagi00001@gmail.com>
2026-01-27 15:57:53 +0530

    Create truthy.js

---
+++ topics/controlfow.js/truthy.js
@@ -0,0 +1,23 @@
+// // falsy values 
+// false , 0 , -0 , NaN , BigInt 0n , null , undefined , "" 
+// truthy values 
+// "0" , "false" , " " ,[] , {} ,funtion (){}, 
+
+// .length se hum pta laga sakte h hmare array empty h ya nhi 
+ 
+const emptyObj = {}
+if (Object.keys(emptyObj).length === 0 ) {          // aise hum pta laga sakte h hmara objecct empty h ya nhi 
+  console.log(`object is empty`);
+  
+}
+
+// Nullish Coalescing Operator (??):  null , undeffined
+
+let val1;
+// val1 = 5 ?? 10 
+// val1 = null ?? 19    // null ko nhi display krta 
+val1 = undefined ?? 14
+
+
+console.log(val1);
+
