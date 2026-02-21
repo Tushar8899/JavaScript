ee6282667a49eae12041a8dd5bf4e5df29140a64
Tushar8899 <tushartyagi00001@gmail.com>
2026-02-21 22:31:21 +0530

    Update promises.js

--- topics/Advance Topic/promises.js
+++ topics/Advance Topic/promises.js
@@ -1,1 +1,14 @@
-const promiseOne = new Promise()
+const promiseOne = new Promise(function(resolve, reject) {              // hum  promise k andar ek funtion likhte h
+  // Do an async task
+  // DB calls
+  // network call 
+  setTimeout ( function (){
+    console.log('async task is completed');
+    resolve()
+  }, 1000)
+})
+
+promiseOne.then(function(){                   // then ka direct relation resolve se hota h 
+  console.log("promise consumed");
+  
+})
