c7e0719cadebd00246acb9bcd081c028e4b2ba8a
Tushar8899 <tushartyagi00001@gmail.com>
2026-02-26 22:08:51 +0530

    Update oop.js

--- topics/OOP and classes/oop.js
+++ topics/OOP and classes/oop.js
@@ -5,11 +5,15 @@ const user = {
 
    getUserDetail: function() {
   // console.log("get user details from database");
-    console.log(`Username: ${this.username}`);      // yha pe this keyword ka matlab h ki vo isi object ki bat kr rha h , agar this nhi lagatte to ye bhar k context samajhta h 
+    console.log(`Username: ${this.username}`);      // yha pe this keyword ka matlab h ki vo isi object ki bat kr rha h , agar this nhi lagatte to ye bhar k context samajhta h , this ka matlab current context hota h 
     
    }
 }
 
 //console.log(user.username);
-console.log(user.getUserDetail());
+//console.log(user.getUserDetail());
 
+
+//constructor funtion 
+const promiseOne = new Promise()      // is new ka matlab constructor function
+const date = new Date()
