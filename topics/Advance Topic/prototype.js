bff2d40dcaf2d5eac6630085a54232cb19ad1dd5
Tushar8899 <tushartyagi00001@gmail.com>
2026-02-28 22:31:17 +0530

    Update prototype.js

--- topics/Advance Topic/prototype.js
+++ topics/Advance Topic/prototype.js
@@ -1,1 +1,15 @@
-// javascript ka default behaviour prototypel behaviour h , isi ko inheritance bolte h  
+// javascript ka default behaviour prototypel behaviour h , isi ko inheritance bolte h 
+
+function multipleBy5(num) {
+  return num*5
+}
+
+multipleBy5.power = 2 
+
+console.log(multipleBy5(5));
+console.log(multipleBy5.power);
+console.log(multipleBy5.prototype);
+
+// javascript me har ek chij object hoti h , function to function hota hi h but object ki terha bhi use kr sakte h 
+
+
