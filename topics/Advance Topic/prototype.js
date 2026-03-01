31dbaa6b394b9402091614c7d4ec6fafbe2f52c6
Tushar8899 <tushartyagi00001@gmail.com>
2026-03-01 15:53:25 +0530

    Update prototype.js

--- topics/Advance Topic/prototype.js
+++ topics/Advance Topic/prototype.js
@@ -29,4 +29,40 @@ createUser.prototype.printMe = function(){
 const milk = new createUser("milk", 35)                 // yha pe properties to aai h , par bataya nhi h isko ki ye properties aai h , isko btane k liye new ka use krenge 
 const coffee = createUser("coffee", 50) 
 
-milk.printMe()               
+milk.printMe()       
+
+// javascript  new keyword k through constructor function deti h 
+
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+let myHeros = ['thor', 'spiderman']
+
+let heroPower = {
+  thor: 'hammer',
+  spiderman: 'sling',
+
+  getSpiderPower: function(){
+    console.log(`spidy power is ${this.spiderman}`);
+    
+  }
+}
+
+Object.prototype.Tushar = function(){                          // yha pe tushar ko directly object me inject kiya h through prototype 
+  console.log(` tushar is present in all objects `);
+  
+}
+
+Array.prototype.heyTushar = function(){                      // yha pe heyTushar method ko directly array me inject kiya h through prototype 
+  console.log(` hello i am tushar `);
+  
+} 
+
+myHeros.Tushar() 
+heroPower.heyTushar()       // yha pe error aaiga kyuki humne heyTushar array me inject kiya to usko hum object me acces nhi kr sakte 
+
+
+// inheritance 
+// har object ka apna alag instance hota h 
+// aajkal .setPrototypeOf se inheritance krte h but phle __proto__ se inhertance krte the  ( isko yaad rakhna h )
